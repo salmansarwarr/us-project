5a6ad87d9e4e352b524cf78e1ad6149f651afebd
Muhammad Salman Sarwar <muhammadsalmansarwar32@gmail.com>
2023-09-08 22:34:07 +0500

    update dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -31,16 +31,19 @@ const Dashboard = () => {
             {/* Main Content */}
             <div className="w-4/5 p-4">
                 {/* Existing Projects */}
-                <div className="mb-8">
+                <div className="mb-8 rounded-lg bg-gray-200 h-[40%] shadow-md p-4">
                     <h1 className="text-2xl font-bold mb-4">
                         Existing Projects
                     </h1>
-                    {projects.length == 0 ? (
-                        <p className="text-red-600">No Existing Projects!</p>
+                    {projects.length === 0 ? (
+                        <p className="text-red-600 w-full text-center mt-20">No Existing Projects!</p>
                     ) : (
                         <ul>
                             {projects.map((project, i) => (
-                                <li key={i} className="mb-2 hover:bg-gray-200">
+                                <li
+                                    key={i}
+                                    className="mb-2 hover:bg-gray-300 p-2 rounded"
+                                >
                                     <Link to={`/project/${project}`}>
                                         {project}
                                     </Link>
@@ -51,13 +54,15 @@ const Dashboard = () => {
                 </div>
 
                 {/* Create new project */}
-                <div>
-                    <h1 className="text-2xl font-bold mb-4">
-                        Create New Project
-                    </h1>
-                    <button className="bg-green-500 flex items-center gap-2 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                        <AiOutlinePlus /> Add Project
-                    </button>
+                <div className="flex justify-center items-center h-60 bg-blue-400 rounded-lg">
+                    <div className="flex flex-col justify-center">
+                        <h1 className="text-2xl font-bold mb-4 text-white">
+                            Create New Project
+                        </h1>
+                        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded flex justify-center items-center gap-2">
+                            <AiOutlinePlus /> Add Project
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
